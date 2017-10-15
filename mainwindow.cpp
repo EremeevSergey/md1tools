1975c27399991e8ab91e830ae49d2854f4cfc8c5
EremeevSergey <eremeev_s@lipetsk.ru>
2017-10-15 15:24:30 +0300

    1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -111,7 +111,7 @@ void MainWindow::updateActions()
     bool fl = Printer.Connection->isOpened();
     actOpenPort->setEnabled(!fl);
     actClosePort->setEnabled(fl);
-//    centralWindow->setEnabled(fl);
+    centralWindow->setEnabled(fl);
 }
 
 void MainWindow::slotUpdate   ()
